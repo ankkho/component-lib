17c46ec476c5b4a547d92d9dea3c7a445aae82f0
Ankit Khosla <ankitdevenderkhosla@airasia.com>
2021-01-23 18:38:58 +0530

    added sortbox component, WIP: filterbox component

---
+++ src/components/FilterBox/filterbox.stories.tsx
@@ -0,0 +1,11 @@
+// import React from 'react';
+// import { storiesOf } from '@storybook/react';
+// import { withKnobs } from '@storybook/addon-knobs';
+// import { withSmartKnobs } from 'storybook-addon-smart-knobs';
+
+// import FilterBox from './index';
+
+// storiesOf('FilterBox', module)
+// 	.addDecorator(withSmartKnobs())
+// 	.addDecorator(withKnobs)
+// 	.add('default', () => <FilterBox />);
